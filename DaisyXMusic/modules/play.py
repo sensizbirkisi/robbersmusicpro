c867976b78ad6d93cacef262ae84f9814d90cf82
bodrumlu48 <77246283+bodrumlu48@users.noreply.github.com>
2021-07-02 11:21:18 +0200

    Update play.py

--- DaisyXMusic/modules/play.py
+++ DaisyXMusic/modules/play.py
@@ -69,7 +69,7 @@ def cb_admin_check(func: Callable) -> Callable:
         if cb.from_user.id in admemes:
             return await func(client, cb)
         else:
-            await cb.answer("You ain't allowed!", show_alert=True)
+            await cb.answer("Bu işleme İzin Verilmiyor!", show_alert=True)
             return
 
     return decorator
